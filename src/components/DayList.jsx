42070e93cf8f64d54d6793f12af1216a41f706b7
JettScythe <jettscythe@hotmail.com>
2021-06-08 18:23:54 -0300

    add day / interview lists & styling

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import DayListItem from './DayListItem'
+import React from "react";
+import DayListItem from "./DayListItem";
 export default function DayList(props) {
-  const { days, day, setDay } = props
-  const dayListDay = days ? days.map((dayListData) => {
-    return (
-    <DayListItem key={dayListData.id} name={dayListData.name} spots={dayListData.spots} setDay={setDay} selected={dayListData.name === props.day}/>
-    )}) : "There are no available days"
-  return (
-    <ul>
-    {dayListDay}
-    </ul>
-  )
+  const { days, day, setDay } = props;
+  const dayListDay = days
+    ? days.map((dayListData) => {
+        return (
+          <DayListItem
+            key={dayListData.id}
+            name={dayListData.name}
+            spots={dayListData.spots}
+            setDay={setDay}
+            selected={dayListData.name === props.day}
+          />
+        );
+      })
+    : "There are no available days";
+  return <ul>{dayListDay}</ul>;
 }
---
+++ src/components/InterviewerList.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import InterviewerListItem from "./InterviewerListItem";
+import "components/InterviewerList.scss";
+
+// PROPS //
+/* interviewers:array - an array of objects containing the information of each interviewer
+interviewer:number - the id of an interviewer
+setInterviewer:function - a function that accepts an interviewer id */
+
+export default function InterviewerList(props) {
+  const interviewer = props.interviewers.map((interviewerData) => {
+        return (
+          <InterviewerListItem
+            key={interviewerData.id}
+            name={interviewerData.name}
+            avatar={interviewerData.avatar}
+            setInterviewer={event => props.onChange(interviewerData.id)}
+            selected={interviewerData.id === props.value}
+          />
+        );
+      });
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewer}</ul>
+    </section>
+  );
+}
